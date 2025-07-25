8b5156f162edf5e1410e46ecd479b9301a6919e6
Nischal <tamangnischal2018@gmail.com>
2025-07-25 14:31:01 +0545

    fibonacci series day 2

---
+++ day2.js
@@ -0,0 +1,5 @@
+//Ques 2 - Fibonacci Number
+//Fibonacci Series -> 0,1,1,2,3,5,8,13,21,34,55,89,144,233...
+// F(0) = 0, F(1) = 1
+// F(n) = F(n-1) + F(n-2) , for n>1
+//input : n =3 ----->>>>> output : 2
