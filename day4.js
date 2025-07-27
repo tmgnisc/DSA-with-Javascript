c474d87eed0e9a719e09f0982f80344b01e0eefb
Nischal <tamangnischal2018@gmail.com>
2025-07-27 13:32:25 +0545

    day 4 question

---
+++ day4.js
@@ -0,0 +1,10 @@
+//Ques 4 - Two sum
+// Given an array of integeres nums and an integer target, 
+//return indices of the two numbers such that they add up to target
+
+//Input: nums = [2,7,11,15], target = 9
+//output: [0,9] (Because nums[0] + nums[1] == 9, we return [0,1])
+//Input : nums = [3,2,4], target = 6
+//output:[1,2]
+
+
