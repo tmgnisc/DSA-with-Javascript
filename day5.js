5f285c99fdcfce2c6b5811c922d779f4b533fc51
Nischal <tamangnischal2018@gmail.com>
2025-07-28 14:08:34 +0545

    question for day 5

---
+++ day5.js
@@ -0,0 +1,10 @@
+//Ques 5 - Best Time to buy and sell stocks
+//You are given an array prices where prices[i] is the price of a given stock
+//on the ith day
+//You want to maximize your profit by choosing a single day to buy one stock
+//and choosing a different day in the future to sell that stock. 
+//Return the maximum profit, if you cannot achieve any profit, return 0
+
+//input: price = [7,1,5,3,6,4] ------>>>>> output: 5
+//input: price = [7,6,4,3,1] -------->>>>> output: 0
+
